7f6e6c5aa67d3f3fbf2c9011479875c09428049d
11rachelh <11rachelh@gmail.com>
2025-12-03 01:06:14 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pymongo import MongoClient
+import os
 
 app = FastAPI()
 
+# 讀取 Render 的環境變數
+MONGODB_URI = os.environ.get("MONGODB_URI")
+
+# 連線 MongoDB
+client = MongoClient(MONGODB_URI)
+db = client["emogo"]  # 可以改成你自己的 DB 名
+collection = db["records"]  # 可以改成你自己的 collection
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def home():
+    return {"message": "EmoGo backend is running"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/export")
+def export_data():
+    data = list(collection.find({}, {"_id": 0}))  # _id 不要給助教看
+    return JSONResponse(content=data)
